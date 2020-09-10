5e89351b7342e7d7a8cb096542e4fd908137cb09
pranavp1616 <pranavp1616@gmail.com>
2020-09-10 16:58:07 +0530

    postgres changes

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+// Use import later (ES6)
 const express = require('express');
 const bodyParser= require('body-parser');
+const { Client } = require('pg');
+
+
 const app = express();
 const PORT = process.env.PORT || 3000; // process.env.PORT for production and 3000 for development
 
+// POSTGRES DB connection
+const pg_client = new Client ({
+    user : 'postgres',
+    host : 'localhost',
+    database : 'MY_SNAPSHARE_DB',
+    password : 'postgres',
+    port : 5432
+})
+pg_client.connect();
+
+
+// *********************************************
+//              REST API start
+// *********************************************
 app.get('/', function(req,res){
     res.json('REST API');
 })
-
 
 
 // photopost CRUD
@@ -18,5 +35,8 @@ app.delete('/api/photopost/delete/:pk', function(req,res){
     res.json('delete');
 })
 
+// *********************************************
+// *********************************************
 
+// start server
 app.listen(PORT, function(){ console.log('server listening...'); })
