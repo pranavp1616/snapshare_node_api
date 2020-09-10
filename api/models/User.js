bf864c894a0940fe6bd61313e888c6a42a721d91
pranavp1616 <pranavp1616@gmail.com>
2020-09-11 00:43:40 +0530

    typo

--- api/models/User.js
+++ api/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema(
     {
-        _id : mongoose.Types.ObjectId,
+        _id : mongoose.Schema.Types.ObjectId,
         username : String,
         email : String,
         password  : String
