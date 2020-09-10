0d4585c4c5cdd400a7a44646ab5743f5976485e3
pranavp1616 <pranavp1616@gmail.com>
2020-09-11 04:48:41 +0530

    email validation

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -4,7 +4,11 @@ const UserSchema = mongoose.Schema(
     {
         _id : mongoose.Schema.Types.ObjectId,
         username : { type:String, required:true },
-        email : { type:String, required:true },
+        email : { 
+            type:String, 
+            required:true, 
+            match : /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        },
         password  : { type:String, required:true },
         date_created : mongoose.Schema.Types.Date
     }
