59a35bcd6054e6192c75def71d89659ba6648fe5
pranavp1616 <pranavp1616@gmail.com>
2020-09-11 00:17:17 +0530

    mongodb atlas clodu connection successful

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 const photoRoutes = require('./api/routes/PhotoPost');
 const likeRoutes = require('./api/routes/LikePost');
 const commentRoutes = require('./api/routes/CommentPost');
 const userRoutes = require('./api/routes/User');
+
+const PW = process.env.MONGO_ATLAS_PW || 'iostream'; 
+mongoose.connect('mongodb+srv://pranav:'+ PW +'@cluster0.hr6hw.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser : true
+    } 
+).then( () => console.log('********Connection Successful********')).catch(err => console.log(err));
+
 
 // all API routes
 const API_STRING = '/api';
