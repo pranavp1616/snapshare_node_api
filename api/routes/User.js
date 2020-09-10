b0ddc538a8205e7ea3bf14e728de03774e774603
pranavp1616 <pranavp1616@gmail.com>
2020-09-11 04:36:18 +0530

    user validation

--- api/routes/User.js
+++ api/routes/User.js
@@ -5,27 +5,31 @@ const mongoose = require('mongoose');
 
 const multer = require('multer');
 const upload = multer();
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');   - HASH password (Add later)
 
 const UserModel = require('../models/UserModel');
 
 router.post('/register', upload.none(), function(req,res){
-//    console.log(req.body.username + req.body.email  + req.body.password);
-//  var hash_pwd = bcrypt.hash(req.body.password,10);              // HASH password and store (add this later)
-    const userObj = new UserModel({
-        _id : new mongoose.Types.ObjectId(),
-        username : req.body.username,
-        email : req.body.email,
-        password : req.body.password,
-        date_created : Date.now()
+    UserModel.findOne({'username':req.body.username}).exec()
+    .then( function(user){ 
+        if(user) 
+            res.status(500).json({response:'error user exists'});
+        else {
+            const userObj = new UserModel({
+                _id : new mongoose.Types.ObjectId(),
+                username : req.body.username,
+                email : req.body.email,
+                password : req.body.password,
+                date_created : Date.now()
+            });
+            userObj.save()
+            .then(  function(){ res.status(201).json({response:'success'})}    )            
+            .catch( function(){ res.status(500).json({response:'error validation'})});
+        } 
     });
-    userObj.save()
-    .then(  function(){ res.status(201).json({response:'success'})}    )
-    .catch( function(){ res.status(500).json({response:'error'})});
 });
 
 router.post('/login', upload.none(), function(req,res){
-//    console.log(req.body.username + req.body.password);
     UserModel.findOne({ username: req.body.username }).exec()
     .then( function(db_res) {
         if(req.body.password == db_res.password) res.status(200).json({ response:'loggedin'});
@@ -34,7 +38,6 @@ router.post('/login', upload.none(), function(req,res){
     .catch( function(err) { 
         res.status(200).json({response:'username doesnt exist'})
     });
-
 });
 
 module.exports = router;
