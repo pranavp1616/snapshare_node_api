89b690da735853eabe510cf66890da0b8b64ab65
pranavp1616 <pranavp1616@gmail.com>
2020-09-11 18:51:26 +0530

    login register fixed

--- api/routes/User.js
+++ api/routes/User.js
@@ -10,37 +10,47 @@ const upload = multer();
 const UserModel = require('../models/UserModel');
 
 router.post('/register', upload.none(), function(req,res){
-    UserModel.findOne({'username':req.body.username})
+    UserModel.findOne({username:req.body.username})
     .exec()
-    .then( function(db_user){ 
+    .then( function(db_user){
         if(db_user) 
             return res.status(500).json({response:'error user exists'});
-        else {
-            const userObj = new UserModel({
-                _id : new mongoose.Types.ObjectId(),
-                username : req.body.username,
-                email : req.body.email,
-                password : req.body.password,
-                date_created : Date.now(),
-                auth_token : req.body.username+'12345'
-            });
-            userObj.save()
-            .then(  function(){ return res.status(201).json({response:'success'})}    )            
-            .catch( function(){ return res.status(500).json({response:'error validation'})});
-        } 
+        else 
+            return _createAndSaveNewUser(req,res);
     });
 });
 
 router.post('/login', upload.none(), function(req,res){
-    UserModel.findOne({ username: req.body.username })
+    UserModel.findOne({username:req.body.username})
     .exec()
-    .then( function(db_user) {
-        if(req.body.password == db_user.password) return res.status(200).json({ response:'success',auth_token:db_user.auth_token});
-        else return res.status(200).json({response:'incorrect password'});
+    .then( function(db_user){
+        if(db_user){
+            if(req.body.password == db_user.password) 
+                return res.status(200).json({ response:'success',auth_token:db_user.auth_token});
+            else 
+                return res.status(200).json({response:'incorrect password'});    
+        }
+        else
+            return res.status(200).json({response:'user doesnt exist'});
     })
-    .catch( function(err) { 
-        return res.status(200).json({response:'username doesnt exist'})
-    });
+    .catch(function(err){
+        return res.status(500).json({response:'server error'});
+    })
 });
+
+function _createAndSaveNewUser(req,res){
+    const userObj = new UserModel({
+        _id : new mongoose.Types.ObjectId(),
+        username : req.body.username,
+        email : req.body.email,
+        password : req.body.password,
+        date_created : Date.now(),
+        auth_token : req.body.username+'12345'
+    });
+    userObj
+    .save()
+    .then(  function(){ return res.status(201).json({response:'success'})   })            
+    .catch( function(){ return res.status(500).json({response:'error validation'})  });
+}
 
 module.exports = router;
