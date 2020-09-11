1b1f44c3a4534f90c182aa9f84167be45ec15ae3
pranavp1616 <pranavp1616@gmail.com>
2020-09-11 07:08:14 +0530

    auth token check fixed

--- api/helperFunctions/IsAuthenticatedMiddleware.js
+++ api/helperFunctions/IsAuthenticatedMiddleware.js
@@ -1,4 +1,16 @@
+const UserModel = require('../models/UserModel');
+const getTokenFromHeader = require('./getTokenFromHeader');
+
 module.exports = function(req,res,next) {
     console.log('auth_token check');
-    next();
+    UserModel.findOne({auth_token:getTokenFromHeader(req)}).exec()
+    .then(function(user){
+        if(user)
+            next();
+        else
+            return res.status(401).json({response:'Not authorized'});
+    })
+    .catch(function(){
+        return res.status(401).json({response:'Not authorized'})
+    });
 } 
