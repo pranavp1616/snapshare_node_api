41528d2aab58f6d6a7f0af5e5bb2042712d31324
pranavp1616 <pranavp1616@gmail.com>
2020-09-12 03:39:23 +0530

    home

--- api/routes/Home-feed.js
+++ api/routes/Home-feed.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const IsAuthenticated = require('../helperFunctions/IsAuthenticatedMiddleware');
+const PhotoPostModel = require('../models/PhotoPostModel');
+
+const POST_PER_PAGE = 2;
 
 router.get('/page/:pageNo', IsAuthenticated, function(req, res) {
     const pageNo = req.params.pageNo;
 
-
-    return res.status(200).json({'response':pageNo});
+    PhotoPostModel
+    .find()
+    .sort({ date_created: -1 })
+    .limit(POST_PER_PAGE)
+    .select('_id uploaded_by image hashtags date_created')
+    .exec()
+    .then(function(data){
+        return res.status(200).json(data);
+    })
+    .catch(function(){ return res.status(500).json({'response':'error'}); });
+    
 });
 
 module.exports = router;
