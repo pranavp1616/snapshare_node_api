b21b37b10269dbf098448c9fefb6b48535e2c8ee
pranavp1616 <pranavp1616@gmail.com>
2020-09-13 02:10:09 +0530

    friend page fix

---
+++ api/routes/Friend-feed.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const IsAuthenticated = require('../helperFunctions/IsAuthenticatedMiddleware');
+const getAllPhotoposts = require('../helperFunctions/getAllPhotoposts');
+const UserModel = require('../models/UserModel');
+
+router.get('/:friendName/page/:pageNo', IsAuthenticated, function(req, res) {
+    UserModel.findOne({username:req.params.friendName})
+    .exec()
+    .then((userObj)=>{
+        if(userObj){
+            getAllPhotoposts({uploaded_by:userObj._id}, req.params.pageNo)
+            .then((data) => {
+                return res.status(200).json(data);
+            })
+            .catch((err) => {
+                return res.status(500).json(err);
+            });    
+        }else{
+            return res.status(200).json([]);
+        }
+    })
+    .catch((err)=>{ return res.status(500).json(err)});
+
+});
+
+module.exports = router;
