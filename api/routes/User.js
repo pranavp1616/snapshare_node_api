53ce458fbc2b0b49092296f8e68fdc0a7c08af3c
pranavp1616 <pranavp1616@gmail.com>
2020-09-12 23:26:43 +0530

    user login and register

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const mongoose = require('mongoose');
-
 const multer = require('multer');
 const upload = multer();
 const bcrypt = require('bcrypt');
@@ -10,58 +8,56 @@ const bcrypt = require('bcrypt');
 const UserModel = require('../models/UserModel');
 
 router.post('/register', upload.none(), function(req, res) {
-    UserModel.findOne({
-            username: req.body.username
-        })
+    UserModel.findOne({ username: req.body.username })
         .exec()
-        .then(function(db_user) {
-            if (db_user)
-                return res.status(500).json({
-                    'response': 'error',
-                    'message': 'Username not available'
-                });
-            else {
-                bcrypt.hash(req.body.password, 10, function(err, hash_password) {
-                    return _createAndSaveNewUser(req, res, hash_password);
-                })
-            }
-        });
+        .then((findOne_result) => { return registerUser(req,res,findOne_result) })
+        .catch((err)=> { return res.status(500).json('server error') });
 });
 
 router.post('/login', upload.none(), function(req, res) {
-    UserModel.findOne({
-            username: req.body.username
-        })
+    UserModel.findOne({ username: req.body.username })
         .exec()
-        .then(function(db_user) {
-            if (db_user) {
-                bcrypt.compare(req.body.password, db_user.password, function(err, result) {
-                    if (result)
-                        return res.status(200).json({
-                            'response': 'success',
-                            'auth_token': db_user.auth_token
-                        });
-                    else
-                        return res.status(200).json({
-                            'response': 'error',
-                            'message': 'Your password was incorrect'
-                        });
-                })
-            } else
-                return res.status(200).json({
-                    'response': 'error',
-                    'message': 'The username you entered does not belong to an account'
-                });
-        })
-        .catch(function(err) {
-            return res.status(500).json({
-                'response': 'error',
-                'message': 'Server Error'
-            });
-        })
+        .then((findOne_result) => { return loginUser(req,res,findOne_result) })
+        .catch((err) => { return res.status(500).json('server error') });
 });
 
-function _createAndSaveNewUser(req, res, hash_password) {
+function registerUser(req,res,result){
+    if (result)
+        return res.status(500).json({ 
+            'response': 'error',
+            'message': 'Username not available'
+        });
+    else {
+        bcrypt.hash(req.body.password, 10)
+        .then((hash_password) => { return createAndSaveNewUser(req, res, hash_password); })
+        .catch((err)=>{ console.log(err) });
+    }
+}
+
+function loginUser(req,res,db_user) {
+    if (db_user) {
+        bcrypt.compare(req.body.password, db_user.password)
+        .then((result) => {
+            if (result)
+                return res.status(200).json({
+                    'response': 'success',
+                    'auth_token': db_user.auth_token
+                });
+            else
+                return res.status(200).json({
+                    'response': 'error',
+                    'message': 'Your password was incorrect'
+                });
+        })
+        .catch((err)=>{ console.log(err); });
+    } else
+        return res.status(200).json({
+            'response': 'error',
+            'message': 'The username you entered does not belong to an account'
+        });
+}
+
+function createAndSaveNewUser(req, res, hash_password) {
     const userObj = new UserModel({
         _id: new mongoose.Types.ObjectId(),
         username: req.body.username,
@@ -75,13 +71,13 @@ function _createAndSaveNewUser(req, res, hash_password) {
     });
     userObj
         .save()
-        .then(function() {
+        .then(() => {
             return res.status(201).json({
                 'response': 'success',
                 'message': 'Account created successfully. Please login'
             })
         })
-        .catch(function() {
+        .catch(() => {
             return res.status(500).json({
                 'response': 'error',
                 'message': 'Validation error. Please try again'
