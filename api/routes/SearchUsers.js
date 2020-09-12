9a9d9be1a475575932a4d2660398297ca3b910ff
pranavp1616 <pranavp1616@gmail.com>
2020-09-13 02:37:40 +0530

    search

--- api/routes/SearchUsers.js
+++ api/routes/SearchUsers.js
@@ -8,8 +8,12 @@ router.get('/:pattern/page/:pageNo', IsAuthenticated, function(req, res) {
     const pattern = req.params.pattern;
     UserModel.find({ 'username': new RegExp(pattern,'i') })
     .exec()
-    .then((result)=> { 
-        return res.status(200).json(result); 
+    .then((result)=> {
+        var search_result = [];
+        const len = result.length;
+        for(var i=0; i<len; i++)
+            search_result.push(result[i].username);
+        return res.status(200).json(search_result); 
     })
     .catch((err)=> { return res.status(500).json(err); })
 });
